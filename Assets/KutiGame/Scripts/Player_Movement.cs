a4e9eee89e236cac3000239b5badb9a1b82c166a
lbluem <lukas.buesen@gmail.com>
2023-05-25 15:31:16 +0200

    Integrierung von unserem letzten Game Stand
    Assets, Szenen eingearbeitet in die "Game.unity" Szene.
    Build läuft auf Handy (auf Kuti noch nicht getestet)

---
+++ Assets/KutiGame/Scripts/Player_Movement.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public Rigidbody2D myRB;
+
+    public float speed = 8;
+    private float moveDirection;
+    // flippt das Sprite 
+    private bool turned = false;
+    public float jumpStrength = 9;
+    private int jumpDirection = 1;
+    private bool isJumping = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRB.velocity = new Vector2(speed*moveDirection, myRB.velocity.y);
+
+        if (!isJumping)
+        {
+            if(KutiInput.GetKutiButtonDown(EKutiButton.P1_MID)||KutiInput.GetKutiButtonDown(EKutiButton.P2_MID))
+            {
+                myRB.velocity = Vector2.up * jumpStrength * jumpDirection;
+                isJumping = !isJumping;
+            }
+        }
+
+         if (KutiInput.GetKutiButtonDown(EKutiButton.P1_LEFT) || KutiInput.GetKutiButtonDown(EKutiButton.P2_RIGHT))
+        {
+            moveDirection = -1;
+        }else if (KutiInput.GetKutiButtonDown(EKutiButton.P1_RIGHT) || KutiInput.GetKutiButtonDown(EKutiButton.P2_LEFT))
+        {
+            moveDirection = 1;
+        }else if(KutiInput.GetKutiButtonUp(EKutiButton.P1_LEFT)||KutiInput.GetKutiButtonUp(EKutiButton.P1_RIGHT)||
+                KutiInput.GetKutiButtonUp(EKutiButton.P2_LEFT)||KutiInput.GetKutiButtonUp(EKutiButton.P2_RIGHT))
+        {
+            moveDirection = 0;
+        }
+
+        if(!turned)
+        {
+            if(myRB.position.y > 1)
+            {
+                GravityTurn();
+            }
+        }else
+        {
+            if (myRB.position.y < -1)
+            {
+                GravityTurn();
+            }
+        }
+
+    }
+
+
+    void GravityTurn()
+    {
+        myRB.gravityScale *= -1;
+        gameObject.transform.Rotate(180,0,0);
+        turned = !turned;
+        jumpDirection *= -1;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.CompareTag("Ground"))   
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) 
+    {
+        if(other.gameObject.CompareTag("Ground"))    
+        {
+            isJumping = true;
+        }
+    }
+}
