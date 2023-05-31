49f8dff277c07a5ff8fbbd0c0ac9eeb888aea6cf
StrangerTNY <2tonytu@gmail.com>
2023-05-31 11:54:19 +0200

    levelübergang

---
+++ Assets/KutiGame/Scripts/goal_handler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class goal_handler : MonoBehaviour
+{
+    [SerializeField]
+    public string NextLevelName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+{
+    SceneManager.LoadScene(NextLevelName, LoadSceneMode.Single);
+}
+}
