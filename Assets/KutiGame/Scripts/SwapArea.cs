e62ca0c0f548fe9c1cab37f12db060d9ff1841f2
lbluem <lukas.buesen@gmail.com>
2023-07-12 21:14:56 +0200

    Swippity; bewegende Plattform Anpassung
    Swippity gehen nur in eine Richtung;
    Moving Platforms haben jetzt eine üppigere Collision Box

--- Assets/KutiGame/Scripts/SwapArea.cs
+++ Assets/KutiGame/Scripts/SwapArea.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class SwapArea : MonoBehaviour
 {
+    [SerializeField] private bool directionIsUp;
+    private bool turned;
+
+
     // Start is called before the first frame update
 
     void Start()
     { 
+        turned = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Movement>().turned;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        turned = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Movement>().turned;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+
         if(other.tag == "Player")
         {
-            FindObjectOfType<AudioManager>().Play("PlayerTurn");
-            other.GetComponent<Player_Movement>().GravityTurn();
+            if((directionIsUp && !turned)||!directionIsUp && turned)
+            {
+                FindObjectOfType<AudioManager>().Play("PlayerTurn");
+                other.GetComponent<Player_Movement>().GravityTurn();
+            } /* else if(!directionIsUp && turned)
+            {
+                FindObjectOfType<AudioManager>().Play("PlayerTurn");
+                other.GetComponent<Player_Movement>().GravityTurn();
+            } */
+
+            
         }
     }
 }
