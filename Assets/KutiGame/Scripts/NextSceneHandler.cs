fd1d9eef2eab5ff5c2740a18f0f365578df5d513
lbluem <lukas.buesen@gmail.com>
2024-02-23 13:58:22 +0100

    1 Bit Level Erweiterungen
    FPS Counter fürs Testing;
    Charakter Animations Anpassungen;
    Nächstes Level Loader neu erstellt.
    1Bit_ver3.apk

---
+++ Assets/KutiGame/Scripts/NextSceneHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneHandler : MonoBehaviour
+{
+
+    //public int LevelToLoad;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            // lädt Level Szene die im Editor angegeben ist
+            //SceneManager.LoadScene(LevelToLoad, LoadSceneMode.Single);
+            
+            // lädt nächste Szene nach Build Index
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+        }
+    }
+}
