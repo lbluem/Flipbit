7283c3bc983578c769ee4f7e859a6d4778161c90
lbluem <lukas.buesen@gmail.com>
2024-03-04 13:47:31 +0100

    Box 1 in Level 5 verschoben
    und Vorbereitung für Easter Eggs

---
+++ Assets/KutiGame/Scripts/SecretFinder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SecretFinder : MonoBehaviour
+{
+    // SecretLifter GameObject has to be the child of a SecretPath Tilemap
+    private Tilemap secretTilemap;
+
+    private void Start() 
+    {
+        secretTilemap = GetComponentInParent<Tilemap>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Player")
+        {
+            secretTilemap.color = new Color(1f,1f,1f,0.2f);
+        }    
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if(other.tag == "Player")
+        {
+            secretTilemap.color = new Color(1f,1f,1f,1f);
+        }  
+    }
+}
