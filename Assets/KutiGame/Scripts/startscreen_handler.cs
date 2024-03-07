fa61088ef8b499b5e9fdc99d3146972c76cf33df
lbluem <lukas.buesen@gmail.com>
2024-03-07 12:49:35 +0100

    Credits Szene Setup

--- Assets/KutiGame/Scripts/startscreen_handler.cs
+++ Assets/KutiGame/Scripts/startscreen_handler.cs
@@ -20,7 +20,7 @@ public class startscreen_handler : MonoBehaviour
         if(DeathCounter.instance != null)
         {
             DeathCounter.instance.resetDeathCount();
-            Debug.Log("Death Counter resetted");
+            Debug.Log("startscreen_handler: Death Counter resetted");
         }
         //playerObject = GameObject.Find("Player");
         //playerObject.GetComponent<Player_Movement>().enabled = false;
