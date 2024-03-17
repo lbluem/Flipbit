560b1753f9b8ee13f94d923dc4de26a0e0fe73ab
lbluem <lukas.buesen@gmail.com>
2024-03-17 15:42:52 +0100

    Fehlermeldung Behebung und Farbanpassungen
    Fehlermeldungen im ersten Level behoben;
    Deko Farben verstärkt für sattere Farben auf dem Kuti

--- Assets/KutiGame/Scripts/startscreen_handler.cs
+++ Assets/KutiGame/Scripts/startscreen_handler.cs
@@ -58,48 +58,53 @@ public class startscreen_handler : MonoBehaviour
     }
 
     void Update (){
-        if(KutiInput.GetKutiButtonDown(EKutiButton.P1_MID)){
-            player_1 = true;
-            foreach (GameObject button in uiButtons)
-            {
-                /* if(button.name == "button_p1_up"){
-                   button.GetComponent<SpriteRenderer>().color = Color.green;
-                } */
-                
-            }
-        }else if(KutiInput.GetKutiButtonDown(EKutiButton.P2_MID)){
-            player_2 = true;
-            foreach (GameObject button in uiButtons)
-            {
-                /* if(button.name == "button_p2_up"){
-                   button.GetComponent<SpriteRenderer>().color = Color.green;
-                } */
-                
-            }
-        }
 
-        if(player_1 || player_2){
-            if(myUIGroup.alpha >= 0){
-                myUIGroup.alpha -= Time.deltaTime;
-            }
-            foreach (GameObject button in uiButtons)
-            {
-                Color colorTmp = button.GetComponent<SpriteRenderer>().color;
-                if(colorTmp.a >= 0){
-                    colorTmp.a -= Time.deltaTime;
-                    button.GetComponent<SpriteRenderer>().color = colorTmp;
+        if(uiButtons != null)
+        {
+            if(KutiInput.GetKutiButtonDown(EKutiButton.P1_MID)){
+                player_1 = true;
+                foreach (GameObject button in uiButtons)
+                {
+                    /* if(button.name == "button_p1_up"){
+                    button.GetComponent<SpriteRenderer>().color = Color.green;
+                    } */
+                    
+                }
+            }else if(KutiInput.GetKutiButtonDown(EKutiButton.P2_MID)){
+                player_2 = true;
+                foreach (GameObject button in uiButtons)
+                {
+                    /* if(button.name == "button_p2_up"){
+                    button.GetComponent<SpriteRenderer>().color = Color.green;
+                    } */
+                    
                 }
             }
-            //playerObject.GetComponent<Player_Movement>().enabled = true;
-            Destroy(GameObject.Find("StartScreen"), 2);
 
-            // Diese If Abfrage wird trotzdem sie zerstört wurde immer noch unzählige male
-            // ausgeführt, d.h. ich kann keinen Sound abspielen da auch dieser dann
-            // bis zu tausende male abgespielt wird
-            if (playSound)
-            {
-                //FindObjectOfType<AudioManager>().Play("MenuButton");
-                playSound = false;
+            if(player_1 || player_2){
+                if(myUIGroup.alpha >= 0){
+                    myUIGroup.alpha -= Time.deltaTime;
+                }
+                foreach (GameObject button in uiButtons)
+                {
+                    Color colorTmp = button.GetComponent<SpriteRenderer>().color;
+                    if(colorTmp.a >= 0){
+                        colorTmp.a -= Time.deltaTime;
+                        button.GetComponent<SpriteRenderer>().color = colorTmp;
+                    }
+                }
+                //playerObject.GetComponent<Player_Movement>().enabled = true;
+                Destroy(GameObject.Find("StartScreen"), 2);
+
+                // Diese If Abfrage wird trotzdem sie zerstört wurde immer noch unzählige male
+                // ausgeführt, d.h. ich kann keinen Sound abspielen da auch dieser dann
+                // bis zu tausende male abgespielt wird
+                if (playSound)
+                {
+                    //FindObjectOfType<AudioManager>().Play("MenuButton");
+                    playSound = false;
+                }
+
             }
         }
     }
