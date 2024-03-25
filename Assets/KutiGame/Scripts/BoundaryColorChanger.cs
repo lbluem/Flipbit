6bc2c489227d2d669bf49f31ce089999a0d90f83
StrangerTNY <2tonytu@gmail.com>
2024-03-25 13:37:04 +0100

    changed boundary color and added new script for color change
    all levels except 6,9,10 (need adjustments still) have new boundary color and a new script handles the color change for the boundaries

---
+++ Assets/KutiGame/Scripts/BoundaryColorChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class BoundaryColorChanger : MonoBehaviour
+{
+
+    public Player_Movement playerMovement;
+    public Tilemap tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerMovement != null && tilemap != null)
+        {
+            if (playerMovement.turned)
+            {
+                ChangeTilemapAlpha(1f); // Set alpha to 1
+            }
+            else
+            {
+                ChangeTilemapAlpha(0f); // Set alpha to 0
+            }
+        }
+    }
+
+    void ChangeTilemapAlpha(float alpha)
+    {
+        Color color = tilemap.color;
+        color.a = alpha;
+        tilemap.color = color;
+    }
+}
