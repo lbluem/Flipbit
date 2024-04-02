e8875655158831b57ca1700c328ed76e76dfe0c2
lbluem <lukas.buesen@gmail.com>
2024-04-02 15:50:31 +0200

    Updated Logo with Flipping

---
+++ Assets/KutiGame/Scripts/LogoFlip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoFlip : MonoBehaviour
+{
+    
+
+
+    void Start()
+    {
+
+        // Event prerequisites
+        Player_Movement player_Movement = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_Movement>();
+        player_Movement.OnGravityChange += ChangeTurned;
+    }
+
+    private void ChangeTurned(bool turnedFromPlayer)
+    {
+        transform.eulerAngles = turnedFromPlayer ? new Vector3(0,0,180) : new Vector3(0,0,0);
+    }
+}
